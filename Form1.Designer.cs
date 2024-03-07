0061521ed2f3cea2059e50eea8d916885742a746
Naveen Kumar <naveenkumaramujuru@gmail.com>
2024-03-07 12:47:26 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace UDP_Asynchronous
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(39, 105);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(255, 359);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(339, 105);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(254, 359);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(741, 105);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(319, 359);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(1104, 105);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(259, 359);
+            this.richTextBox4.TabIndex = 3;
+            this.richTextBox4.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1025, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "127.0.0.1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Server",
+            "Client"});
+            this.comboBox1.Location = new System.Drawing.Point(603, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(755, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Client";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1402, 496);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace UDP_Asynchronous
+{
+    //The commands for interaction between the server and the client
+    enum Command
+    {
+        Login,      //Log into the server
+        Logout,     //Logout of the server
+        Message,    //Send a text message to all the chat clients
+        List,       //Get a list of users in the chat room from the server
+        Null        //No command
+    }
+    public partial class Form1 : Form
+    {
+        Socket serverSocket;
+        public Socket clientSocket; //The main client socket
+        public EndPoint epServer;   //The EndPoint of the server
+
+        byte[] byteData = new byte[1024];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                CheckForIllegalCrossThreadCalls = false;
+
+                //We are using UDP sockets
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                //Assign the any IP of the machine and listen on port number 1000
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 1000);
+
+                //Bind this address to the server
+                serverSocket.Bind(ipEndPoint);
+
+                IPEndPoint ipeSender = new IPEndPoint(IPAddress.Any, 1000);
+                //The epSender identifies the incoming clients
+                EndPoint epSender = (EndPoint)ipeSender;
+
+                //Start receiving data
+                serverSocket.BeginReceiveFrom(byteData, 0, byteData.Length, SocketFlags.None, ref epSender, new AsyncCallback(OnReceive), epSender);
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);       //IP address of the server machine
+                IPAddress ipAddress = IPAddress.Parse(textBox1.Text);            //Server is listening on port 1000
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 1000);
+                epServer = (EndPoint)ipEndPoint;
+
+                Data msgToSend = new Data();
+                byteData = msgToSend.ToByte();
+
+                clientSocket.BeginSendTo(byteData, 0, byteData.Length, SocketFlags.None, epServer, new AsyncCallback(OnSend), null);
+                byteData = new byte[1024];
+                //Start listening to the data asynchronously
+                clientSocket.BeginReceiveFrom(byteData, 0, byteData.Length, SocketFlags.None, ref epServer, new AsyncCallback(OnReceive), null);
+            }
+        }
+        private void OnReceive(IAsyncResult ar)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                try
+                {
+                    IPEndPoint ipeSender = new IPEndPoint(IPAddress.Any, 1000);
+                    EndPoint epSender = (EndPoint)ipeSender;
+
+                    serverSocket.EndReceiveFrom(ar, ref epSender);
+
+                    //Transform the array of bytes received from the user into an object.
+                    Data msgReceived = new Data(byteData);
+
+                    //We will send this object in response the users request
+                    Data msgToSend = new Data();
+                    msgToSend.cmdCommand = msgReceived.cmdCommand;
+                    msgToSend.strName = msgReceived.strName;
+
+                    richTextBox1.Text = msgReceived.strMessage + "\r\n";
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToCaret();
+                    serverSocket.BeginReceiveFrom(byteData, 0, byteData.Length, SocketFlags.None, ref epSender, new AsyncCallback(OnReceive), epSender);
+
+                    Data msgToSend1 = new Data();
+                    richTextBox2.Text = DateTime.UtcNow.ToString();
+                    msgToSend1.strMessage = richTextBox2.ToString();
+                    byte[] message;
+                    message = msgToSend1.ToByte();
+                    serverSocket.BeginSendTo(message, 0, message.Length, SocketFlags.None, epSender, new AsyncCallback(OnSend), epSender);
+                    Thread.Sleep(100);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "SGSServerUDP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                try
+                {
+                    clientSocket.EndReceive(ar);
+
+                    //Convert the bytes received into an object of type Data
+                    Data msgReceived = new Data(byteData);
+
+                    richTextBox3.Text = msgReceived.strMessage + "\r\n";
+                    richTextBox3.SelectionStart = richTextBox3.Text.Length;
+                    richTextBox3.ScrollToCaret();
+
+                    //Start listening to receive more data from the user
+                    clientSocket.BeginReceiveFrom(byteData, 0, byteData.Length, SocketFlags.None, ref epServer, new AsyncCallback(OnReceive), null);
+                    Thread.Sleep(10);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "SGSclient: ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void OnSend(IAsyncResult ar)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                try
+                {
+                    //IPEndPoint ipeSender = new IPEndPoint(IPAddress.Any, 1000);
+                    //EndPoint epSender = (EndPoint)ipeSender;
+                    //Data msgToSend = new Data();
+                    //byte[] message;
+                    //txt_Send.Text = DateTime.UtcNow.ToString();
+                    //msgToSend.strMessage = txt_Send.ToString();
+                    //message = msgToSend.ToByte();
+                    //serverSocket.BeginSendTo(message, 0, message.Length, SocketFlags.None, epSender, new AsyncCallback(OnSend), epSender);
+                    Thread.Sleep(100);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "SGSServerUDP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                try
+                {
+                    //clientSocket.EndSend(ar);
+                    Data msgToSend = new Data();
+                    richTextBox4.Text = DateTime.Now.ToString();
+
+                    msgToSend.strMessage = richTextBox4.Text;
+                    //msgToSend.cmdCommand = Command.Message;
+
+                    byte[] byteData = msgToSend.ToByte();
+
+                    //Send it to the server
+                    clientSocket.BeginSendTo(byteData, 0, byteData.Length, SocketFlags.None, epServer, new AsyncCallback(OnSend), null);
+                    Thread.Sleep(1000);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "SGSclient: ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        //The data structure by which the server and the client interact with each other.
+        class Data
+        {
+            //Default constructor
+            public Data()
+            {
+                this.cmdCommand = Command.Null;
+                this.strMessage = null;
+                this.strName = null;
+            }
+            //Converts the bytes into an object of type Data
+            public Data(byte[] data)
+            {
+                //The first four bytes are for the Command
+                this.cmdCommand = (Command)BitConverter.ToInt32(data, 0);
+
+                //The next four store the length of the name
+                int nameLen = BitConverter.ToInt32(data, 4);
+
+                //The next four store the length of the message
+                int msgLen = BitConverter.ToInt32(data, 8);
+
+                //This check makes sure that strName has been passed in the array of bytes
+                if (nameLen > 0)
+                    this.strName = Encoding.UTF8.GetString(data, 12, nameLen);
+                else
+                    this.strName = null;
+
+                //This checks for a null message field
+                if (msgLen > 0)
+                    this.strMessage = Encoding.UTF8.GetString(data, 12 + nameLen, msgLen);
+                else
+                    this.strMessage = null;
+            }
+            //Converts the Data structure into an array of bytes
+            public byte[] ToByte()
+            {
+                List<byte> result = new List<byte>();
+
+                //First four are for the Command
+                result.AddRange(BitConverter.GetBytes((int)cmdCommand));
+
+                //Add the length of the name
+                if (strName != null)
+                    result.AddRange(BitConverter.GetBytes(strName.Length));
+                else
+                    result.AddRange(BitConverter.GetBytes(0));
+
+                //Length of the message
+                if (strMessage != null)
+                    result.AddRange(BitConverter.GetBytes(strMessage.Length));
+                else
+                    result.AddRange(BitConverter.GetBytes(0));
+
+                //Add the name
+                if (strName != null)
+                    result.AddRange(Encoding.UTF8.GetBytes(strName));
+
+                //And, lastly we add the message text to our array of bytes
+                if (strMessage != null)
+                    result.AddRange(Encoding.UTF8.GetBytes(strMessage));
+
+                return result.ToArray();
+            }
+            public string strName;      //Name by which the client logs into the room
+            public string strMessage;   //Message text
+            public Command cmdCommand;  //Command type (login, logout, send message, etcetera)
+        }
+    }
+}
